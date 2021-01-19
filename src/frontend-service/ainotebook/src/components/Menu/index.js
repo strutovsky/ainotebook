8f8e664dd4e3637e6cc9e3335c887183b7ba9a87
Naholiuk Dmitriy <nagoluk1626@gmail.com>
2021-01-19 12:26:59 +0200

    Universal component for adding to list

--- src/frontend-service/ainotebook/src/components/Menu/index.js
+++ src/frontend-service/ainotebook/src/components/Menu/index.js
@@ -7,10 +7,11 @@ import {
     PieChartOutlined,
     DesktopOutlined,
     ContainerOutlined,
-    MailOutlined, BookOutlined, PlusOutlined,
+    MailOutlined, BookOutlined, PlusOutlined, PaperClipOutlined, SettingOutlined,
 } from '@ant-design/icons';
 
 import MenuStyles from './menu.module.css'
+import Add from '../../common/adding';
 
 const { SubMenu } = Menu;
 
@@ -31,12 +32,7 @@ class MainMenu extends React.Component {
 
     addNoteBook = (e) => {
         this.props.setAddingMode(false)
-        this.setState({notebookValue: ''})
-        this.props.AddBook(this.state.notebookValue)
-    }
-
-    nootebookChangeHandler = (e) => {
-        this.setState({notebookValue: e.target.value})
+        this.props.AddBook(e.target.value)
     }
 
     toggleCollapsed = () => {
@@ -65,46 +61,35 @@ class MainMenu extends React.Component {
                         {this.props.notebooks.map(item => (<Menu.Item key={item.id} icon={<BookOutlined />}>
                             {item.name}
                         </Menu.Item>))}
+
+                        <Add
+                            addingMode={this.props.addingMode}
+                            setAddingMode={this.props.setAddingMode}
+                            placeholder={"Add notebook"}
+                            add={this.addNoteBook}
+                        />
                     </Menu.ItemGroup>
 
-                    {!this.props.addingMode && <div className={MenuStyles.tempNoteboook}
-                               key={'add'}
-                               onClick={() => this.props.setAddingMode(true)}
-                    >
-                        <PlusOutlined /> Add notebook
-                    </div>}
 
 
-                    {this.props.addingMode &&  <Menu.Item className={MenuStyles.tempNoteboook}
-                               key={'temp'} icon={<BookOutlined /> }
-                               disabled={true}
-                    >
-
-                        <Input ref={this.addBookRef}
-                               placeholder="Enter name of notebook"
-                               value={this.state.notebookValue}
-                               autoFocus={true}
-                               onChange={this.nootebookChangeHandler}
-                               onBlur={this.addNoteBook}/>
-
-                    </Menu.Item>}
-
-                    <Menu.ItemGroup title={"Quick notice"}>
-                        <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-                            <Menu.Item key="5">Option 5</Menu.Item>
-                            <Menu.Item key="6">Option 6</Menu.Item>
-                            <Menu.Item key="7">Option 7</Menu.Item>
-                            <Menu.Item key="8">Option 8</Menu.Item>
+                    <Menu.ItemGroup title={"Sticks"}>
+                        <SubMenu key="sub1" icon={<PaperClipOutlined />} title="Stick">
+                            <Menu.Item key="5">Page 1</Menu.Item>
+                            <Menu.Item key="5">Page 2</Menu.Item>
+                            <Menu.Item key="5">Page 3</Menu.Item>
                         </SubMenu>
                     </Menu.ItemGroup>
-                    <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-                        <Menu.Item key="9">Option 9</Menu.Item>
-                        <Menu.Item key="10">Option 10</Menu.Item>
-                        <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="11">Option 11</Menu.Item>
-                            <Menu.Item key="12">Option 12</Menu.Item>
+
+                    <Menu.ItemGroup title={"Setting and other"}>
+                        <SubMenu key="sub2" icon={<SettingOutlined />} title="Setting" o>
+                            <Menu.Item key="9">Option 9</Menu.Item>
+                            <Menu.Item key="10">Option 10</Menu.Item>
+                            <SubMenu key="sub3" title="Submenu">
+                                <Menu.Item key="11">Option 11</Menu.Item>
+                                <Menu.Item key="12">Option 12</Menu.Item>
+                            </SubMenu>
                         </SubMenu>
-                    </SubMenu>
+                    </Menu.ItemGroup>
                 </Menu>
             </div>
         );
