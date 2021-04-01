2f7578d9d2440329adec58faa3c14877ff514cdc
strutovsky <max.strutovsky@peoly.com>
2021-04-01 22:15:59 +0300

    Add updating notebook's name

--- src/server/routes/notebooks.py
+++ src/server/routes/notebooks.py
@@ -1,9 +1,10 @@
 from server import db
 from flask import request, Response, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import cross_origin
 from bson import ObjectId
 from . import routes
 import datetime
+
 
 class Pages(db.EmbeddedDocument):
     _id = db.ObjectIdField()
@@ -35,6 +36,10 @@ class Notebook(db.Document):
     def delete_page(self, id):
         self.update(pull__pages___id=id)
 
+    def update_name(self, name):
+        self.name = name
+        self.save()
+
     def to_json(self):
         return {"id": str(self.id), "name": self.name, "pages": [p.to_json() for p in self.pages]}
 
@@ -42,52 +47,68 @@ class Notebook(db.Document):
 @routes.route("/notebooks", methods=["GET"])
 @cross_origin(supports_credentials=True)
 def get_all_notebooks():
-    ''' Gets all user notebooks '''
+    # Gets all user notebooks
     res = []
     for notebook in Notebook.objects:
         res.append(notebook.to_json())
 
     return jsonify(res)
 
+
 @routes.route("/notebook", methods=["POST"])
 @cross_origin(supports_credentials=True)
 def create_notebook():
-    ''' Creates notebook with given name and empty page '''
+    # Creates notebook with given name and empty page
     name = request.args.get('name')
     notebook = Notebook(name=name).save()
     notebook.add_new_page("New page", "", "")
     return notebook.to_json()
 
+
 @routes.route("/notebook", methods=["DELETE"])
 @cross_origin(supports_credentials=True)
 def delete_notebook():
-    ''' Deletes notebook '''
-    id = request.args.get('id')
+    # Deletes notebook
+    id = request.args.get('nid')
     notebook = Notebook.objects.get_or_404(id=id)
     notebook.delete()
     return Response(status=200)
 
+
 @routes.route("/notebook", methods=["GET"])
 @cross_origin(supports_credentials=True)
 def get_notebook_by_id():
-    ''' Gets user notebook by id '''
-    id = request.args.get('id')
+    # Gets user notebook by id
+    id = request.args.get('nid')
     notebook = Notebook.objects.get_or_404(id=id)
     return notebook.to_json()
+
+
+@routes.route("/notebook", methods=["PUT"])
+@cross_origin(supports_credentials=True)
+def update_notebook():
+    # Updates notebook name
+    nid = request.args.get('nid')
+    name = request.args.get('name')
+    notebook = Notebook.objects.get_or_404(id=nid)
+    notebook.update_name(name)
+    return Response(status=200)
+
 
 @routes.route("/page", methods=["POST"])
 @cross_origin(supports_credentials=True)
 def create_page_of_notebook():
-    ''' Creates a notebook page by id '''
+    # Creates a notebook page by id
     body = request.get_json()
     notebook = Notebook.objects.get(id=body["nid"])
     notebook.add_new_page(title=body["title"], body=body["body"], metadata=body["metadata"])
     return Response(status=200)
 
+
 @routes.route("/page", methods=["GET"])
 @cross_origin(supports_credentials=True)
 def get_page_of_notebook():
-    ''' Gets a page of notebook '''
+    # Gets a page of notebook
     nid = request.args.get('nid')
     pid = request.args.get('pid')
     notebook = Notebook.objects.get_or_404(id=nid)
@@ -99,33 +120,15 @@ def get_page_of_notebook():
 
     return page.to_json() if page else Response(status=404)
 
+
 @routes.route("/page", methods=["DELETE"])
 @cross_origin(supports_credentials=True)
 def delete_page():
-    ''' Deletes page by id '''
+    # Deletes page by id
     nid = request.args.get('nid')
     pid = request.args.get('pid')
     notebook = Notebook.objects.get_or_404(id=nid)
     notebook.delete_page(pid)
     return Response(status=200)
 
-
-
-@routes.route("/notebook/<id>", methods=["PUT"])
-@cross_origin(supports_credentials=True)
-def update_notebook(id):
-    ''' Updates notebook name '''
-    body = request.get_json()
-    notebook = Notebook.objects.get_or_404(id=id)
-    notebook.name = body['name']
-    notebook.save()
-    return Response(status=200)
-
-
-
-
-
-
-# TODO: update https://stackoverflow.com/questions/12387478/updating-a-list-of-embedded-documents-in-mongoengine
-# https://gist.github.com/pingwping/92219a8a1e9d44e1dd8a
 # TODO: update page of notebook
