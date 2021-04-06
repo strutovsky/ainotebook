5316c1b457c3eccce85980f21ef988d9fbe6d8e1
Max Strutovsky <maxstrutovskyi@gmail.com>
2021-04-06 05:04:51 +0300

    Add setting up name of file as user_id

--- src/storage/storage.py
+++ src/storage/storage.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify, flash, request, redirect, url_for
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import os
 from werkzeug.utils import secure_filename
+from werkzeug.exceptions import BadRequest
+
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -10,7 +12,7 @@ UPLOAD_FOLDER = "./static/profiles"
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
 
@@ -18,33 +20,24 @@ app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/upload", methods=['GET', 'POST'])
+@app.route("/upload", methods=['POST'])
 @cross_origin(supports_credentials=True)
 def upload_image():
-    if request.method == "POST":
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
+    if 'file' not in request.files:
+        return BadRequest("No file part")
 
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+    file = request.files['file']
+    user_id = request.form['user_id']
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], filename))
-            return redirect("http://localhost/"+str(filename))
+    if file.filename == '':
+        return BadRequest("No selected file")
 
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    if file and allowed_file(file.filename):
+        filename = user_id + "." + file.filename.rsplit('.', 1)[1].lower()
+        filename = secure_filename(filename)
+        file.save(os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], filename))
+        return jsonify({"filename": filename}), 200
+    return BadRequest("Bad image")
 
 
 
